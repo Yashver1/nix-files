692e8093e9c2cf5571897f4e08af8e94c4df0d84
yashver <yashvershori@gmail.com>
2025-10-06 23:24:23 +0800

    flake update: nixos

--- flake.nix
+++ flake.nix
@@ -15,7 +15,6 @@
     { self, nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
