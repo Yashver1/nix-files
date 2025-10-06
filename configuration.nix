6d2bc7d4f93278c8239a2ec1d382ef7f011aa108
yashver <yashvershori@gmail.com>
2025-10-06 16:40:00 +0800

    flake update: nixos

--- configuration.nix
+++ configuration.nix
@@ -63,15 +63,10 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Enable Nvidia
-
-  hardware.opengl = {
-    enable = true;
-  };
-
   hardware.graphics = {
     enable = true;
   };
