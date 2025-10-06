c1b3eced1163d574cd79d656ef97e9b8f82cc1c7
yashver <yashvershori@gmail.com>
2025-10-06 15:24:03 +0800

    flake update: nixos

--- home.nix
+++ home.nix
@@ -140,7 +140,6 @@
     enable = true;
     prefix = "C-\\\\";
     plugins = with pkgs.tmuxPlugins; [
-      tpm
       tmux-sensible
       vim-tmux-navigator
       minimal-tmux-status
@@ -152,8 +151,6 @@
       set -g @minimal-tmux-bg "#f2ba7b"
       set -g @minimal-tmux-status "bottom"
 
-      set-environment -g TMUX_PLUGIN_MANAGER_PATH '~/.config/tmux/plugins/'
-
       set -g @vim_navigator_mapping_left 'C-h'
       set -g @vim_navigator_mapping_right 'C-l'
       set -g @vim_navigator_mapping_up 'C-k'
@@ -164,8 +161,6 @@
       bind -n M-j resize-pane -D 5
       bind -n M-h resize-pane -L 5
       bind -n M-l resize-pane -R 5
-
-      run '~/.config/tmux/plugins/tpm/tpm'
     '';
 
   };
