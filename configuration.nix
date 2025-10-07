2d55851c4c91bbe33ec16fba0410dd20b2913081
yashver <yashvershori@gmail.com>
2025-10-07 21:37:08 +0800

    flake update: nixos

--- configuration.nix
+++ configuration.nix
@@ -35,7 +35,20 @@
   # ZSH
   users.defaultUserShell = pkgs.zsh;
   programs.zsh.enable = true;
+
+  # LD
   programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    fuse3
+    icu
+    nss
+    openssl
+    curl
+    expat
+
+  ];
 
   # Flakes
   nix.settings.experimental-features = [
