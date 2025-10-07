73dad911166b0d536fd928e6ba3d213bfccffbf6
yashver <yashvershori@gmail.com>
2025-10-08 00:43:35 +0800

    flake update: nixos

--- home.nix
+++ home.nix
@@ -114,10 +114,22 @@
       INSERT_MODE_INDICATOR=" "
     '';
 
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
+
     oh-my-zsh = {
       enable = true;
       plugins = [
-        "nix-shell"
         "git"
         "z"
         "vi-mode"
