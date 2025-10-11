333d8ee3b3ffebe5f843d6d976e1787b10ccfb39
Yashver1 <yashvershori@gmail.com>
2025-10-12 02:16:26 +0800

    update: combine darwin and os flakes

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Nixos config flake";
+  description = "My Hosts";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -9,21 +9,38 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-  };
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
 
-  outputs =
-    inputs@{ self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-    in
+   };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, ... }:
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        system = system;
+        system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
           ./arceus/configuration.nix
           inputs.home-manager.nixosModules.default
         ];
+      };
+
+      darwinConfigurations."Yashvers-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        specialArgs = {inherit inputs;};
+        modules = [ 
+            ./giratina/configuration.nix
+            inputs.home-manager.darwinModules.default
+            nix-homebrew.darwinModules.nix-homebrew 
+            {
+              nix-homebrew = {
+                enable = true;
+                user = "yashver";
+                autoMigrate = true;
+              };
+            }
+          ];
       };
     };
 }
---
+++ giratina/homebrew.nix
@@ -0,0 +1,105 @@
+{
+  homebrew = {
+        enable = true;
+
+        brews = [
+          "rust-analyzer"
+          "libpq"
+          "ffmpeg"
+          "uv"
+          "nvm"
+          "automake"
+          "boost"
+          "catch2"
+          "cmake"
+          "cppcheck"
+          "dos2unix"
+          "fd"
+          "ffuf"
+          "fmt"
+          "fswatch"
+          "gh"
+          "git"
+          "go"
+          "gobuster"
+          "imagemagick"
+          "inetutils"
+          "just"
+          "lsd"
+          "lua-language-server"
+          "luarocks"
+          "make"
+          "mas"
+          "neovim"
+          "ninja"
+          "php"
+          "pnpm"
+          "protobuf"
+          "pyenv"
+          "r"
+          "gcc"
+          "radare2"
+          "ripgrep"
+          "rp"
+          "tmux"
+          "wireshark"
+          "yazi"
+          "zoxide"
+          "fzf"
+          "unixodbc" 
+          "openssl" 
+          "mysql" 
+          "zstd" 
+          "zlib" 
+          "pkg-config"
+          "lazygit"
+          "ruby" 
+          "pipx" 
+
+          "llvm@20"
+        ];
+
+        casks = [
+          "slack"
+          "ghostty"
+          "aerospace"
+          "dbeaver-community"
+          "font-fira-code"
+          "font-hack-nerd-font"
+          "hopper-disassembler"
+          "mactex"
+          "ngrok"
+          "pgadmin4"
+          "playcover-community"
+          "stats"
+        ];
+
+        masApps = {
+          "Amphetamine"       = 937984704;
+          "Dynamic Wallpaper" = 1453504509;
+          "GarageBand"        = 682658836;
+          "Goodnotes"         = 1444383602;
+          "iMovie"            = 408981434;
+          "Keynote"           = 409183694;
+          "Klack"             = 6446206067;
+          "Microsoft Excel"   = 462058435;
+          "Microsoft Word"    = 462054704;
+          "Numbers"           = 409203825;
+          "Pages"             = 409201541;
+          "PDFgear"           = 6469021132;
+          "SiteSucker"        = 442168834;
+          "Telegram"          = 747648890;
+          "WeChat"            = 836500024;
+          "Wireless@SGx"      = 1449928544;
+          "Yoink"             = 457622435;
+        };
+
+        taps = [
+          "nikitabobko/tap"
+        ];
+
+        onActivation.cleanup = "zap";
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
+    };
+}
