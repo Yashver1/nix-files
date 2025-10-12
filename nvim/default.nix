a65978a183ca66541e1ba018c4f5221aaab440ec
yashver <yashvershori@gmail.com>
2025-10-13 02:18:41 +0800

    flake update: nixos

--- nvim/default.nix
+++ nvim/default.nix
@@ -1,77 +1,11 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   programs.nixvim = {
     enable = true;
     imports = [
       ./keymaps.nix
-      ./treesitter.nix
-      ./toggleterm.nix
-      ./themes.nix
-      ./lazygit.nix
-      ./lualine.nix
-      ./dashboard.nix
-      ./bufferline.nix
-      ./snacks.nix
-      ./gitsigns.nix
-      ./whichkey.nix
-      ./hlchunk.nix
-      ./yanky.nix
-      ./autopairs.nix
-      ./blink-cmp.nix
-      ./tmux-navigator.nix
-      ./smear-cursor.nix
-      ./lsp/conform.nix
-      ./lsp/fidget.nix
-      ./lsp/lsp.nix
-      ./nix-develop.nix
-      ./aerial.nix
-      ./autosave.nix
-      ./notify.nix
-      ./barbecue.nix
-      ./noice.nix
-      ./neoscroll.nix
-      ./markview.nix
-      ./zen-mode.nix
-      ./yazi.nix
-      ./wtf.nix
-      ./ts-comments.nix
-      ./timerly.nix
-      ./treesj.nix
-      ./web-devicons.nix
-    ];
-
-    globals = {
-      mapleader = " ";
-    };
-
-    opts = {
-      number = true;
-      relativenumber = true;
-      shiftwidth = 2;
-      tabstop = 2;
-      wrap = false;
-      swapfile = false; # Undotree
-      backup = false; # Undotree
-      undofile = true;
-      hlsearch = false;
-      incsearch = true;
-      termguicolors = true;
-      scrolloff = 8;
-      signcolumn = "yes";
-      updatetime = 50;
-      foldlevelstart = 99;
-    };
-    extraPackages = with pkgs; [
-      stylua # Lua formatter
-      csharpier # C# formatter
-      nixfmt-rfc-style # Nix formatter
-      golangci-lint # Go linter
-      shellcheck # Shell script linter
-      eslint_d # JavaScript/TypeScript linter
-      netcoredbg # C# debugger
-      asm-lsp # Assembly LSP
-      delve # Go debugger
-
+      ./settings.nix
+      ./plugins
     ];
   };
 
