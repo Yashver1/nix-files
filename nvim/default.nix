4453558545c53f53f11cfcdc0f0b16f7c8d11ad4
yashver <yashvershori@gmail.com>
2025-10-12 22:55:42 +0800

    flake update: nixos

--- nvim/default.nix
+++ nvim/default.nix
@@ -3,9 +3,7 @@
   programs.nixvim = {
     enable = true;
     options.termguicolors = true;
-
-    plugins.gruvbox.enable = true;  # load the plugin
-    colorscheme = "gruvbox";        # set it as the active scheme
+    colorschemes.gruvbox.enable = true;
   };
 }
 
