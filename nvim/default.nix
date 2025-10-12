785283600fb2bd875fc6600e6183c13282817907
yashver <yashvershori@gmail.com>
2025-10-13 00:02:15 +0800

    flake update: nixos

--- nvim/default.nix
+++ nvim/default.nix
@@ -12,22 +12,6 @@
 
   programs.nixvim = {
     enable = true;
-    defaultEditor = true;
-
-    nixpkgs.useGlobalPackages = true;
-
-    # performance = {
-    #   combinePlugins = {
-    #     enable = true;
-    #     standalonePlugins = [
-    #       "hmts.nvim"
-    #       "neorg"
-    #       "nvim-treesitter"
-    #     ];
-    #   };
-    #   byteCompileLua.enable = true;
-    # };
-
     viAlias = true;
     vimAlias = true;
 
