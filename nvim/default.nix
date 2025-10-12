2a51823fce4d0d9b54958a998dd4f297a11715c4
yashver <yashvershori@gmail.com>
2025-10-13 01:56:47 +0800

    flake update: nixos

--- nvim/default.nix
+++ nvim/default.nix
@@ -40,51 +40,49 @@
       ./web-devicons.nix
     ];
 
-    config = {
-      globals = {
-        mapleader = " ";
-      };
-
-      opts = {
-        number = true;
-        colorcolumn = "80";
-        relativenumber = true;
-        shiftwidth = 2;
-        tabstop = 2;
-        wrap = false;
-        swapfile = false; # Undotree
-        backup = false; # Undotree
-        undofile = true;
-        hlsearch = false;
-        incsearch = true;
-        termguicolors = true;
-        scrolloff = 8;
-        signcolumn = "yes";
-        updatetime = 50;
-        foldlevelstart = 99;
-      };
-      extraPackages = with pkgs; [
-        # base
-        nerd-fonts.jetbrains-mono # Font
-        fzf
-        ripgrep
-        fd
-        # Formatters
-        stylua # Lua formatter
-        csharpier # C# formatter
-        nixfmt-rfc-style # Nix formatter
-        # Linters
-        golangci-lint # Go linter
-        shellcheck # Shell script linter
-        eslint_d # JavaScript/TypeScript linter
-        # Debuggers
-        netcoredbg # C# debugger
-        asm-lsp # Assembly LSP
-        bashdb # Bash debugger
-        delve # Go debugger
-
-      ];
+    globals = {
+      mapleader = " ";
     };
 
+    opts = {
+      number = true;
+      colorcolumn = "80";
+      relativenumber = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      wrap = false;
+      swapfile = false; # Undotree
+      backup = false; # Undotree
+      undofile = true;
+      hlsearch = false;
+      incsearch = true;
+      termguicolors = true;
+      scrolloff = 8;
+      signcolumn = "yes";
+      updatetime = 50;
+      foldlevelstart = 99;
+    };
+    extraPackages = with pkgs; [
+      # base
+      nerd-fonts.jetbrains-mono # Font
+      fzf
+      ripgrep
+      fd
+      # Formatters
+      stylua # Lua formatter
+      csharpier # C# formatter
+      nixfmt-rfc-style # Nix formatter
+      # Linters
+      golangci-lint # Go linter
+      shellcheck # Shell script linter
+      eslint_d # JavaScript/TypeScript linter
+      # Debuggers
+      netcoredbg # C# debugger
+      asm-lsp # Assembly LSP
+      bashdb # Bash debugger
+      delve # Go debugger
+
+    ];
   };
+
 }
