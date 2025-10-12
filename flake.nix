e627b87caaaf25d3dd5fb652507121e82bbf727b
yashver <yashvershori@gmail.com>
2025-10-12 23:48:01 +0800

    flake update: nixos

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     };
 
     nixvim = {
-      url = "github:nix-community/nixvim";
+      url = "github:nix-community/nixvim/nixos-24.05";
     };
 
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
