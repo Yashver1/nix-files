64c44165aab296d687ba0151fa47c94c623b040a
yashver <yashvershori@gmail.com>
2025-10-12 23:05:10 +0800

    flake update: nixos

--- nvim/default.nix
+++ nvim/default.nix
@@ -2,18 +2,7 @@
 {
   programs.nixvim = {
     enable = true;
-
-    # using the wrapper => use the `.config = { ... }` style
     nixpkgs.useGlobalPackages = true;
-    # (alternatively: nixpkgs.pkgs = pkgs;)
-
-    config = {
-      options.termguicolors = true;
-
-      # your nixvim revision doesn't support `colorschemes.*` — use plugins + colorscheme
-      plugins.gruvbox.enable = true;
-      colorscheme = "gruvbox";
-    };
   };
-}
+};
 
