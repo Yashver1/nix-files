a90c58a4b0491708fb384149c28dd7cdb6b63ff9
yashver <yashvershori@gmail.com>
2025-10-12 23:36:32 +0800

    flake update: nixos

--- arceus/home.nix
+++ arceus/home.nix
@@ -10,8 +10,6 @@
   # manage.
   home.username = "yash";
   home.homeDirectory = "/home/yash";
-  useGlobalPkgs = true;
-  useUserPackages = true;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
