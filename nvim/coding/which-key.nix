72da60ca404be0e7af1ad48997bcf68f1f5e035f
yashver <yashvershori@gmail.com>
2025-10-14 00:31:09 +0800

    flake update: nixos

--- nvim/coding/which-key.nix
+++ nvim/coding/which-key.nix
@@ -55,6 +55,52 @@
 					require("which-key").show({ keys = "<c-w>", loop = true })
 				      end, { desc = "Window Hydra Mode (which-key)" })
 
+				local i = {
+				    [" "] = "Whitespace",
+				    ['"'] = 'Balanced "',
+				    ["'"] = "Balanced '",
+				    ["`"] = "Balanced `",
+				    ["("] = "Balanced (",
+				    [")"] = "Balanced ) including white-space",
+				    [">"] = "Balanced > including white-space",
+				    ["<lt>"] = "Balanced <",
+				    ["]"] = "Balanced ] including white-space",
+				    ["["] = "Balanced [",
+				    ["}"] = "Balanced } including white-space",
+				    ["{"] = "Balanced {",
+				    ["?"] = "User Prompt",
+				    _ = "Underscore",
+				    a = "Argument",
+				    b = "Balanced ), ], }",
+				    c = "Class",
+				    d = "Digit(s)",
+				    e = "Word in CamelCase & snake_case",
+				    f = "Function",
+				    g = "Entire file",
+				    i = "Indent",
+				    o = "Block, conditional, loop",
+				    q = "Quote `, \", '",
+				    t = "Tag",
+				    u = "Use/call function & method",
+				    U = "Use/call without dot in name",
+				  }
+				  local a = vim.deepcopy(i)
+				  for k, v in pairs(a) do
+				    a[k] = v:gsub(" including.*", "")
+				  end
+
+				  local ic = vim.deepcopy(i)
+				  local ac = vim.deepcopy(a)
+				  for key, name in pairs({ n = "Next", l = "Last" }) do
+				    i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, ic)
+				    a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, ac)
+				  end
+				  require("which-key").register({
+				    mode = { "o", "x" },
+				    i = i,
+				    a = a,
+				  })
+
 				 '';
 
 		};
