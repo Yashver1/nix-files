a5beefd51713043742e3686c7f8dc5544107b87e
yashver <yashvershori@gmail.com>
2025-10-13 21:16:17 +0800

    flake update: nixos

--- nvim/coding/mini-pairs.nix
+++ nvim/coding/mini-pairs.nix
@@ -6,7 +6,6 @@
       content = ''
         require("mini.pairs").setup({
           modes = { insert = true, command = true, terminal = false },
-          skip_next = [[[%w%%%'%[%"%.%`%$]]],
           skip_ts = { "string" },
           skip_unbalanced = true,
           markdown = true,
--- nvim/default.nix
+++ nvim/default.nix
@@ -4,7 +4,6 @@
     enable = true;
     imports = [
       ./coding
-
     ];
   };
 
