8f3c8d7763321982faac654c7cdb8dd5c3df13b2
yashver <yashvershori@gmail.com>
2025-10-14 01:35:00 +0800

    flake update: nixos

--- nvim/coding/mini-ai.nix
+++ nvim/coding/mini-ai.nix
@@ -1,3 +1,4 @@
+
 {...}:{
 
 plugins.mini-ai = {
@@ -25,24 +26,21 @@ plugins.mini-ai = {
           "^().*()$",
         },
 
-	g = {
-	  a = function()
-	    local start_line, end_line = 1, vim.fn.line("$")
-	    local to_col = math.max(vim.fn.getline(end_line):len(), 1)
-	    return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
-	  end,
-
-	  i = function()
-	    local start_line, end_line = 1, vim.fn.line("$")
+	g = function(ai_type)
+	  local start_line, end_line = 1, vim.fn.line("$")
+	  if ai_type == "i" then
+	    -- Skip first and last blank lines for `i` textobject
 	    local first_nonblank, last_nonblank = vim.fn.nextnonblank(start_line), vim.fn.prevnonblank(end_line)
+	    -- Do nothing for buffer with all blanks
 	    if first_nonblank == 0 or last_nonblank == 0 then
 	      return { from = { line = start_line, col = 1 } }
 	    end
 	    start_line, end_line = first_nonblank, last_nonblank
-	    local to_col = math.max(vim.fn.getline(end_line):len(), 1)
-	    return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
-	  end,
-	},
+	  end
+
+	  local to_col = math.max(vim.fn.getline(end_line):len(), 1)
+	  return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
+	end,
 
        u = ai.gen_spec.function_call(),
        U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }),
@@ -53,3 +51,4 @@ plugins.mini-ai = {
 
 
 }
+
