8f03c59e65743a945e9dec17dd8aeb8b188b4de0
yashver <yashvershori@gmail.com>
2025-10-14 20:29:49 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -149,6 +149,7 @@
     extraGroups = [
       "networkmanager"
       "wheel"
+      "docker"
     ];
     packages = with pkgs; [
     ];
@@ -176,6 +177,10 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+
+  virtualisation.docker = {
+    enable = true;
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
