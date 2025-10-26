a652fcee9175d341a0cfd17af2db7aa3389b0ebd
yashver <yashvershori@gmail.com>
2025-10-27 00:45:33 +0800

    flake update: nixos

--- arceus/home.nix
+++ arceus/home.nix
@@ -910,11 +910,11 @@
         enabled-extensions = [
           pkgs.gnomeExtensions.blur-my-shell.extensionUuid
           pkgs.gnomeExtensions.space-bar.extensionUuid
-          pkgs.gnomeExtensions.forge.extensionUuid
-          # pkgs.gnomeExtensions.just-perfection.extensionUuid
-          # pkgs.gnomeExtensions.user-themes.extensionUuid
-          #
-          # pkgs.gnomeExtensions.rounded-window-corners-reborn.extensionUuid
+          # pkgs.gnomeExtensions.forge.extensionUuid
+          pkgs.gnomeExtensions.just-perfection.extensionUuid
+          pkgs.gnomeExtensions.user-themes.extensionUuid
+          pkgs.gnomeExtensions.rounded-window-corners-reborn.extensionUuid
+
         ];
       };
 
