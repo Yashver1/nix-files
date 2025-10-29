d90a98a9527e8093e88f40fff06e94c1b553dde2
yashver <yashvershori@gmail.com>
2025-10-30 02:27:22 +0800

    flake update: nixos

--- arceus/home.nix
+++ arceus/home.nix
@@ -64,8 +64,10 @@
     # '';
     #
     ".config/custom-oh-my-zsh/themes/robbyrussell2.zsh-theme".text = ''
-      PROMPT="[%F{yellow}$SHLVL%f] %(?:%{$fg_bold[green]%}%1{➜%} :%{$fg_bold[red]%}%1{➜%} ) %{$fg[cyan]%}%c%{$reset_color%}"
+      PROMPT="%(?:%{$fg_bold[green]%}%1{➜%} :%{$fg_bold[red]%}%1{➜%} ) %{$fg[cyan]%}%c%{$reset_color%}"
       PROMPT+=' $(git_prompt_info)'
+      RPROMPT="[%F{yellow}$SHLVL%f]"
+
 
       ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
       ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
