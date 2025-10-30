a15a373d3c888781135849236fee2a6ff0efc910
yashver <yashvershori@gmail.com>
2025-10-31 02:34:03 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -206,6 +206,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    luajitPackages.lua-lsp
     foundry
     lm_sensors
     fancontrol-gui
