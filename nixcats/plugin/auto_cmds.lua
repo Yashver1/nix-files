eae4111b59d1a6e10e3b91e80fe910f97580e55d
yashver <yashvershori@gmail.com>
2025-11-01 01:37:52 +0800

    flake update: nixos

--- nixcats/plugin/auto_cmds.lua
+++ nixcats/plugin/auto_cmds.lua
@@ -1,9 +1,7 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { 
-    "Make*",
-    "make*",
-  },
+  pattern = "make",
   callback = function()
+    vim.opt_local.expandtab = true
     vim.opt_local.softtabstop = 4
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
