6c5e24b347f46d0d3c0adcd09b8338e9f744a24c
yashver <yashvershori@gmail.com>
2025-11-01 01:04:36 +0800

    flake update: nixos

--- nixcats/plugin/options.lua
+++ nixcats/plugin/options.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.g.expandtab = true
-vim.g.softtabstop = 2
-vim.g.tabstop = 2
-vim.g.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
