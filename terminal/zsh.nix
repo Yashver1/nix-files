8da1caad2d4a73d9dc2f91b964c937bd1cde5015
yashver <yashvershori@gmail.com>
2025-11-09 21:09:38 +0800

    flake update: nixos

--- terminal/zsh.nix
+++ terminal/zsh.nix
@@ -49,7 +49,7 @@
       {
         name = "vi-mode";
         src = pkgs.zsh-vi-mode;
-        file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+        file = "zsh-vi-mode.plugin.zsh";
       }
     ];
 
