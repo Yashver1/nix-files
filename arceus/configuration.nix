766af3bc05d6cae242fb6a75252b6e7c241f4875
yashver <yashvershori@gmail.com>
2025-11-09 15:11:46 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -212,6 +212,13 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     libva-utils
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
+    gst_all_1.gst-vaapi # This is the most important one
 
     # NIRI pkgs
     rofi
