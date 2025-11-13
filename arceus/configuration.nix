97f8db19d1742fed7b6bd9b6b79e4700b7cf3573
yashver <yashvershori@gmail.com>
2025-11-13 19:13:55 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -221,6 +221,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    bpftrace
+    ftrace
     lua
     insomnia
     cider-2
