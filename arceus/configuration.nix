61917fdca2f0da38ccffd8e9a6a1ecf9f6d50ec5
yashver <yashvershori@gmail.com>
2025-11-17 23:02:02 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -223,6 +223,10 @@
     defaultEditor = true;
   };
 
+  programs.yazi = {
+    enable = true;
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
