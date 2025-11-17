7e4ad3ce0c30b6ffed38d43c133e4518d523fce7
yashver <yashvershori@gmail.com>
2025-11-18 00:55:18 +0800

    flake update: nixos

--- arceus/home.nix
+++ arceus/home.nix
@@ -128,11 +128,11 @@
   programs.home-manager.enable = true;
 
   # XDG options
-
   xdg = {
     enable = true;
   };
 
+  # Zsh options
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -155,6 +155,14 @@
         ZVM_SYSTEM_CLIPBOARD_ENABLED=true
         ZVM_VI_HIGHLIGHT_BACKGROUND=white
         ZVM_VI_HIGHLIGHT_BACKGROUND=black
+      }
+
+      function y() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+        yazi "$@" --cwd-file="$tmp"
+        IFS= read -r -d \'\' cwd < "$tmp"
+        [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+        rm -f -- "$tmp"
       }
     '';
 
