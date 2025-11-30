d4314d88a0aec0fce2678b2805160bbd2d544da0
yashver <yashvershori@gmail.com>
2025-11-30 18:22:57 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -232,7 +232,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    xar
+    uget
     shadps4
     poppler
     resvg
