b19ce1f77115bafa755468f19f397080aa414716
yashver <yashvershori@gmail.com>
2025-12-24 20:22:38 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -233,6 +233,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    dolphin-emu
     anki-bin
     uget
     shadps4
