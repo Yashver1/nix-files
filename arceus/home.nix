25ad65c630dda0541276b27a6b3cc15c6ff34cd3
yashver <yashvershori@gmail.com>
2026-01-04 01:04:40 +0800

    flake update: nixos

--- arceus/home.nix
+++ arceus/home.nix
@@ -228,20 +228,8 @@
 
   qt = {
     enable = true;
-    platformTheme.name = "qtct";
-    style = {
-      name = "kvantum";
-    };
+    platformTheme.name = "gtk3";
 
-  };
-
-  xdg.configFile = {
-    "Kvantum/kvantum.kvconfig".text = ''
-      [General]
-      theme=GraphiteNordDark
-    '';
-
-    "Kvantum/GraphiteNord".source = "${pkgs.graphite-kde-theme}/share/Kvantum/GraphiteNord";
   };
 
   programs.yazi = {
