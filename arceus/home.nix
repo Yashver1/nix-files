8d77ffa35aea54732cdba54d3c03a638a8f9d425
yashver <yashvershori@gmail.com>
2026-01-04 00:49:40 +0800

    flake update: nixos

--- arceus/home.nix
+++ arceus/home.nix
@@ -330,14 +330,6 @@
       package = pkgs.whitesur-cursors;
     };
 
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-
   };
 
   dconf = {
