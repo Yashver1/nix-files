43b7714f8dfa9ffb25e5144942131d8a4ac5bd0d
yashver <yashvershori@gmail.com>
2026-01-04 14:29:03 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -237,7 +237,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    libreoffice
+    libreoffice-qt
     pavucontrol
     pamixer
     azahar
