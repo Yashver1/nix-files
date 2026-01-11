2ad455fd12242a89a3e606efb71a2c2a32b9fa1d
yashver <yashvershori@gmail.com>
2026-01-11 21:17:07 +0800

    flake update: nixos

--- niri/default.nix
+++ niri/default.nix
@@ -151,10 +151,10 @@
         {
           matches = [ ];
           geometry-corner-radius = {
-            bottom-left = 11.0;
-            bottom-right = 11.0;
-            top-left = 11.0;
-            top-right = 11.0;
+            bottom-left = 8.0;
+            bottom-right = 8.0;
+            top-left = 8.0;
+            top-right = 8.0;
           };
           clip-to-geometry = true;
         }
