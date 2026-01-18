74d78a3a5d22561d62f071dc08671f684c8dc622
yashver <yashvershori@gmail.com>
2026-01-18 11:37:03 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -115,7 +115,7 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
+  # services.displayManager.gdm.enable = true;
   # services.desktopManager.gnome.enable = true;
 
   # Enable Opengl
