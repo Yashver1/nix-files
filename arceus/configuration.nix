e73f8f9f26ca18f1fc20f4109ac0c88068f15ab7
yashver <yashvershori@gmail.com>
2026-01-18 15:09:50 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -256,6 +256,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    gemini-cli
     opencode
     libreoffice-qt-fresh
     cava
