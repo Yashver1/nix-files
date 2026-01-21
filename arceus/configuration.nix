eca84b828ec65d0ad1da301e05e0fb745236a7af
yashver <yashvershori@gmail.com>
2026-01-21 21:37:41 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -246,7 +246,6 @@
       qemu = {
         swtpm.enable = true;
         ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
       };
     };
     spiceUSBRedirection.enable = true;
