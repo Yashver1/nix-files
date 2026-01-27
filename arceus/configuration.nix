51b63ee5011ae518bb42b3fee58718aa7dc74311
yashver <yashvershori@gmail.com>
2026-01-27 20:15:50 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -272,6 +272,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    wget
     lsof
     dnsmasq
     virt-viewer
