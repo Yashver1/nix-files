04c5ee6cf19d7b527a1c20f967d6bfeb8cedb945
yashver <yashvershori@gmail.com>
2026-02-05 22:45:23 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -115,7 +115,7 @@
 
   # Enable the GNOME Desktop Environment.
   # services.displayManager.gdm.enable = true;
-  # services.desktopManager.gnome.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   services.displayManager.ly = {
     enable = true;
