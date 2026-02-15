0ce7f9adb8590b7c4c8b40e80314de829252517b
Yashver1 <yashvershori@gmail.com>
2026-02-15 22:13:17 +0800

    flake update: nix-darwin (mac-pro)

--- giratina/home.nix
+++ giratina/home.nix
@@ -98,7 +98,7 @@
       l = "lsd -l";
       la = "lsd -la";
       ls = "lsd";
-      nb = "sudo darwin-rebuild switch --flake /Users/yashver/.config/nix/";
+      nb = " git -C /Users/yashver/.config/nix add . && git -C /Users/yashver/.config/nix commit -m 'flake update: nix-darwin (mac-pro)' && git -C /Users/yashver/.config/nix push  origin master && sudo darwin-rebuild switch --flake /Users/yashver/.config/nix/";
     };
 
     initContent = ''
