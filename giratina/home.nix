6baf3deb3a23a9a75cbeebe947c3516ca2fa644e
Yashver1 <yashvershori@gmail.com>
2026-02-15 22:34:49 +0800

    flake update: nix-darwin (mac-pro)

--- giratina/home.nix
+++ giratina/home.nix
@@ -56,7 +56,7 @@
     window-padding-balance = true
     window-padding-x = 25
     window-padding-y = 15
-    font-family = "JetBrainsMono"
+    font-family = "JetBrains Mono"
   '';
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
