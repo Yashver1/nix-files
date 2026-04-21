c00e81fc1f3824916f74cfe6e5c243e569e79968
Yashver1 <yashvershori@gmail.com>
2026-04-21 14:38:42 +0800

    flake update: nix-darwin (mac-pro)

--- giratina/home.nix
+++ giratina/home.nix
@@ -116,7 +116,7 @@
       ns = "nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history";
     };
 
-    zprof.enable = true;
+    # zprof.enable = true;
 
     initContent = ''
       zmodload zsh/zprof
