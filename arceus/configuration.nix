a42a9375240ba15325d059b8d10741074bab6ff8
yashver <yashvershori@gmail.com>
2026-05-17 22:39:48 +0800

    flake update: nixos

--- arceus/configuration.nix
+++ arceus/configuration.nix
@@ -302,6 +302,7 @@
   environment.systemPackages = with pkgs; [
     dotnet-sdk
     dust
+    sioyek
     stress-ng
     ghidra-extensions.ret-sync
     checksec
@@ -498,6 +499,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?appstream
+  system.stateVersion = "25.05"; # Did you read the comment?
 
 }
